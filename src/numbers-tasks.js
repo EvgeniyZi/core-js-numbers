e13b6d5d9598a04bcf06a567b05d68a30cd0704d
evgeniy zimagorov <zimagorov.evgeniy@gmail.com>
2025-10-22 11:01:32 +0300

    feat: solve task 4 - calculate distance between two points

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -67,8 +67,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const sideLenghtX = Math.abs(x1) + Math.abs(x2);
+  const sideLenghtY = Math.abs(y1) + Math.abs(y2);
+  return Math.sqrt(sideLenghtX ** 2 + sideLenghtY ** 2);
 }
 
 /**
