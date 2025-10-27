1d6b011b6caa40014d8a8b940891929574c76801
evgeniy zimagorov <zimagorov.evgeniy@gmail.com>
2025-10-27 19:47:33 +0300

    feat: Task #37 solved - returns count of odd numbers from zero to the resulting number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -650,8 +650,14 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let sum = 0;
+  for (let i = 1; i <= Math.abs(number); i += 1) {
+    if (i % 2 === 1) {
+      sum += 1;
+    }
+  }
+  return sum;
 }
 
 module.exports = {
